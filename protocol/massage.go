8b93d5c6332870f07ada0c77168c6d17d5b5f209
marsevilspirit <marsevilspirit@gmail.com>
2024-10-25 02:11:26 +0800

    fix some bugs about massage.go

--- protocol/massage.go
+++ protocol/massage.go
@@ -60,7 +60,8 @@ type Message struct {
 }
 
 func NewMessage() *Message {
-	header := Header([8]byte{})
+	header := Header([12]byte{})
+	header[0] = magicNumber
 	return &Message{
 		Header:   &header,
 		Metadata: make(map[string]string),
@@ -74,7 +75,7 @@ func NewMessage() *Message {
 //
 // h[2]的第8位是MessageType, 第7位是IsHeartbeat, 第6位是IsOneway
 // 第5-3位是CompressType
-type Header [8]byte
+type Header [12]byte
 
 func (h Header) CheckMagicNumber() bool {
 	return h[0] == magicNumber
@@ -102,7 +103,7 @@ func (h Header) IsHeartbeat() bool {
 	return h[2]&0x40 == 0x40
 }
 
-func (h Header) SetHeartbeat(hb bool) {
+func (h *Header) SetHeartbeat(hb bool) {
 	if hb {
 		h[2] = h[2] | 0x40
 	} else {
@@ -114,7 +115,7 @@ func (h Header) IsOneway() bool {
 	return h[2]&0x20 == 0x20
 }
 
-func (h Header) SetOneway(oneway bool) {
+func (h *Header) SetOneway(oneway bool) {
 	if oneway {
 		h[2] = h[2] | 0x20
 	} else {
@@ -126,16 +127,24 @@ func (h Header) CompressType() CompressType {
 	return CompressType((h[2] & 0x1C) >> 2)
 }
 
-func (h Header) SetCompressType(ct CompressType) {
-	h[2] = h[2] | (byte(ct) << 2)
+func (h *Header) SetCompressType(ct CompressType) {
+	h[2] = h[2] | ((byte(ct) << 2) & 0x1C)
+}
+
+func (h Header) MessageStatusType() MessageStatusType {
+	return MessageStatusType(h[2] & 0x03)
+}
+
+func (h *Header) SetMessageStatusType(mt MessageStatusType) {
+	h[2] = h[2] | (byte(mt) & 0x03)
 }
 
 // 0xF0 是 1111 0000
-func (h Header) SerializeType() SerializeType {
+func (h *Header) SerializeType() SerializeType {
 	return SerializeType((h[3] & 0xF0) >> 4)
 }
 
-func (h Header) SetSerializeType(st SerializeType) {
+func (h *Header) SetSerializeType(st SerializeType) {
 	h[3] = h[3] | (byte(st) << 4)
 }
 
@@ -143,7 +152,7 @@ func (h Header) Seq() uint64 {
 	return binary.BigEndian.Uint64(h[4:])
 }
 
-func (h Header) SetSeq(seq uint64) {
+func (h *Header) SetSeq(seq uint64) {
 	binary.BigEndian.PutUint64(h[4:], seq)
 }
 
@@ -151,16 +160,58 @@ func (m Message) Encode() []byte {
 	meta := encodeMetadata(m.Metadata)
 
 	// 4 指明长度
-	l := 8 + (4 + len(meta)) + (4 + len(m.Payload))
+	l := 12 + (4 + len(meta)) + (4 + len(m.Payload))
 
 	data := make([]byte, l)
 	copy(data, m.Header[:])
-	binary.BigEndian.PutUint32(data[8:12], uint32(len(meta)))
+	binary.BigEndian.PutUint32(data[12:16], uint32(len(meta)))
 	copy(data[12:], meta)
-	binary.BigEndian.PutUint32(data[12+len(meta):], uint32(len(m.Payload)))
-	copy(data[16+len(meta):], m.Payload)
+	binary.BigEndian.PutUint32(data[16+len(meta):], uint32(len(m.Payload)))
+	copy(data[20+len(meta):], m.Payload)
 
 	return data
+}
+
+func (m Message) WriteTo(w io.Writer) (int64, error) {
+	var bytes int64
+
+	n, err := w.Write(m.Header[:])
+	if err != nil {
+		return bytes, err
+	}
+
+	bytes = int64(n)
+
+	meta := encodeMetadata(m.Metadata)
+	err = binary.Write(w, binary.BigEndian, uint32(len(meta)))
+	if err != nil {
+		return bytes, err
+	}
+
+	bytes += int64(binary.Size(uint32(len(meta))))
+
+	n, err = w.Write(meta)
+	if err != nil {
+		return bytes, err
+	}
+
+	bytes += int64(n)
+
+	err = binary.Write(w, binary.BigEndian, uint32(len(m.Payload)))
+	if err != nil {
+		return bytes, err
+	}
+
+	bytes += int64(binary.Size(uint32(len(m.Payload))))
+
+	n, err = w.Write(m.Payload)
+	if err != nil {
+		return bytes, err
+	}
+
+	bytes += int64(n)
+
+	return bytes, err
 }
 
 func encodeMetadata(m map[string]string) []byte {
@@ -194,11 +245,11 @@ func decodeMetadata(lenData []byte, r io.Reader) (map[string]string, error) {
 	}
 
 	meta := bytes.Split(data, lineSeparator)
-	if len(meta)%2 != 0 {
+	if len(meta)%2 != 1 {
 		return nil, ErrMetaKVMissing
 	}
 
-	for i := 0; i < len(meta); i = i + 2 {
+	for i := 0; i < len(meta)-1; i = i + 2 {
 		m[util.SliceByteToString(meta[i])] = util.SliceByteToString(meta[i+1])
 	}
 
---
+++ protocol/massage_test.go
@@ -0,0 +1,146 @@
+package protocol
+
+import (
+	"testing"
+	"bytes"
+)
+
+func TestHeader_CheckMagicNumber(t *testing.T) {
+	header := Header([12]byte{})
+	header[0] = magicNumber
+	if !header.CheckMagicNumber() {
+		t.Errorf("expected magic number %x, got %x", magicNumber, header[0])
+	}
+}
+
+func TestHeader_Version(t *testing.T) {
+	header := Header([12]byte{})
+	version := byte(1)
+	header.SetVersion(version)
+	if header.Version() != version {
+		t.Errorf("expected version %d, got %d", version, header.Version())
+	}
+}
+
+func TestHeader_Heartbeat(t *testing.T) {
+	header := Header([12]byte{})
+
+	header.SetHeartbeat(true)
+	if !header.IsHeartbeat() {
+		t.Errorf("expected Heartbeat true, got false")
+	}
+
+	header.SetHeartbeat(false)
+	if header.IsHeartbeat() {
+		t.Errorf("expected Heartbeat false, got true")
+	}
+}
+
+func TestHeader_Oneway(t *testing.T) {
+	header := Header([12]byte{})
+
+	header.SetOneway(true)
+	if !header.IsOneway() {
+		t.Errorf("expected Oneway true, got false")
+	}
+
+	header.SetOneway(false)
+	if header.IsOneway() {
+		t.Errorf("expected Oneway false, got true")
+	}
+}
+
+func TestHeader_CompressType(t *testing.T) {
+	header := Header([12]byte{})
+
+	header.SetCompressType(None)
+	if header.CompressType() != None {
+		t.Errorf("expected compress %d, got %d", None, header.CompressType())
+	}
+
+	header.SetCompressType(Gzip)
+	if header.CompressType() != Gzip {
+		t.Errorf("expected compress %d, got %d", Gzip, header.CompressType())
+	}
+}
+
+func TestHeader_SerializeType(t *testing.T) {
+	header := Header([12]byte{})
+
+	header.SetSerializeType(JSON)
+	if header.SerializeType() != JSON {
+		t.Errorf("expected serialize %d, got %d", JSON, header.SerializeType())
+	}
+
+	header.SetSerializeType(ProtoBuffer)
+	if header.SerializeType() != ProtoBuffer {
+		t.Errorf("expected serialize %d, got %d", ProtoBuffer, header.SerializeType())
+	}
+}
+
+func TestHeader_Seq(t *testing.T) {
+	header := Header([12]byte{})
+
+	seq := uint64(114514)
+	header.SetSeq(seq)
+	if header.Seq() != seq {
+		t.Errorf("expected seq %d, got %d", seq, header.Seq())
+	}
+}
+
+func TestMessage_EncodeDecode(t *testing.T) {
+	req := NewMessage()
+	req.SetVersion(0)
+	req.SetMessageType(Request)
+	req.SetHeartbeat(false)
+	req.SetOneway(false)
+	req.SetCompressType(None)
+	req.SetMessageStatusType(Normal)
+	req.SetSerializeType(JSON)
+
+	req.SetSeq(114514)
+
+	m := make(map[string]string)
+	m["__METHOD"] = "Mrpc.Test"
+	m["__ID"] = "41235123613476347134623"
+	req.Metadata = m
+
+	payload := `{
+		"A": 1,
+		"B": 2,
+	}`
+
+	req.Payload = []byte(payload)
+
+	var buf bytes.Buffer
+	_, err := req.WriteTo(&buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := readMessage(&buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res.SetMessageType(Response)
+
+	if !res.CheckMagicNumber() {
+		t.Errorf("expect MagicNumber true, got flase")
+	}
+
+	if res.Version() != 0 {
+		t.Errorf("expect Version 0, got %d", res.Version())
+	}
+
+	if res.Seq() != 114514 {
+		t.Errorf("expect Seq 114514, got %d", res.Seq())
+	}
+
+	if res.Metadata["__METHOD"] != "Mrpc.Test" || res.Metadata["__ID"] != "41235123613476347134623" {
+		t.Errorf("got wrong metadata: %v", res.Metadata)
+	}
+
+	if string(res.Payload) != payload {
+		t.Errorf("got wrong payload: %v", string(res.Payload))
+	}
+}
